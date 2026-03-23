57a9c4dc071307b7a1efa8d5e5c4603984c393a1
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2026-03-23 14:49:08 +0500

    fix(lint): disabled for require

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 if (process.env.CI !== "true") {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   require("./src/config/env");
 }
 
