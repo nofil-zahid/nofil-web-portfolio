b82f237b5b499dd2f48af7e9168f757ea4ec57cf
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2026-03-23 15:04:27 +0500

    fix(CI): default values added

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 import path from "path";
 
-console.log("CI", process.env.CI);
-
 if (process.env.CI !== "true") {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   require("./src/config/env");
