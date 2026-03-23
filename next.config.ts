3f2ddf40324e934fbd709eb15108452f3b1aaa81
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2026-03-23 14:56:53 +0500

    test thing

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 import path from "path";
 
+console.log("CI", process.env.CI);
+
 if (process.env.CI !== "true") {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   require("./src/config/env");
