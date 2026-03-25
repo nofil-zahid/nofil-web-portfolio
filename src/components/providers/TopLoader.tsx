902b9482ac1f57896271774ddb523e6b044a111e
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2026-03-26 02:19:44 +0500

    fix(TopLoader): resolve hydration mismatch and missing hook dependencies

--- src/components/providers/TopLoader.tsx
+++ src/components/providers/TopLoader.tsx
@@ -1,15 +1,25 @@
 'use client';
+import { useEffect } from "react";
 import { useResponsive } from "@/hooks/core/use-responsive";
 import NextTopLoader from "nextjs-toploader";
+import { useBooleanToggle } from "@/hooks/core/use-boolean-toggle";
 
 export default function TopLoader() {
   const { isMobile } = useResponsive();
+  const { state: mounted, enable } = useBooleanToggle(false);
+
+  useEffect(() => {
+    enable();
+  }, [enable]);
+
+  if (!mounted) return null;
+
   return (
     <NextTopLoader
       color="#0df259"
       initialPosition={0.08}
       crawlSpeed={2000}
-      height={isMobile ? 5 : 3}
+      height={(mounted && isMobile) ? 5 : 3}
       crawl={true}
       showSpinner={false}
       easing="ease-in-out"
