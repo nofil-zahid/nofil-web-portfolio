8ac6834c4d250e6f13e748c96f8ac1802e92c2bb
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2026-08-30 20:04:14 +0500

    feat: new cursor component

--- src/components/providers/CS-Provider.tsx
+++ src/components/providers/CS-Provider.tsx
@@ -1,18 +1,6 @@
 import { ChildrenProps } from '@/types/components';
-import ClickSpark from '../shared/ClickSpark';
+import Cursor from '../shared/Cursor';
 
 export default function CSProvider({ children }: ChildrenProps) {
-  return (
-    <ClickSpark
-      sparkColor="#0df259"
-      sparkSize={10}
-      sparkRadius={15}
-      sparkCount={8}
-      duration={400}
-      easing="ease-out"
-      extraScale={1.0}
-    >
-      {children}
-    </ClickSpark>
-  );
+  return <Cursor duration={400}>{children}</Cursor>;
 }
